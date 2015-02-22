2cb0ca12ff132db62d46d38abd73d0327e6f2474
Luke Olson <luke.olson@gmail.com>
2015-02-21 20:43:32 -0600

    .h parsable

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import re
+
+
 def identify_template(hfile):
     """
     Parameters
@@ -31,6 +34,8 @@ def identify_template(hfile):
         - in addition 'const' and 'void'
     """
 
+    types = ['i', 'I', 't', 'T']
+
     with open(hfile, 'rU') as hfid:
         text = hfid.read()
 
@@ -38,4 +43,45 @@ def identify_template(hfile):
     temp_start = [m.start(0) for m in temp_iter]
 
     ntemp = len(temp_start)
-    re.compile('.*<\s*class\s*([A-Z,a-z]+)
+    classre = re.compile('template.*<(.+)>')
+    funcre = re.compile('template.*<.*>\s*(.+)\s*{')
+    argsre = re.compile('(.+)\s+(.+)\s*\((.+)\)')
+
+    funcs = {}
+    print('[parsing %s]' % hfile)
+    for tstart in temp_start:
+        # class list
+        classes = classre.search(text, tstart).groups(0)[0].strip()
+
+        # function call
+        funccall = funcre.search(text, tstart).groups(0)[0].strip()
+
+        # check classes
+        classes = re.sub('class', '', classes)
+        classes = re.sub('\s', '', classes).split(',')
+        for tid  in classes:
+            if tid not in types:
+                raise ValueError('class type \'%s\' not supported ' % tid +
+                                 'in your header file %s' % hfile) 
+
+        # get the function declaration
+        m = argsre.match(funccall)
+        funcret = m.group(1)
+        funcname = m.group(2)
+        funcargs = m.group(3)
+        args = funcargs.split(',')
+        const = []
+        atype = []
+        for arg in args:
+            if 'const ' in arg:
+                const.append(True)
+            else:
+                const.append(False)
+            arg = arg.replace('const', '').strip()
+            atype.append(arg[0])
+
+        print('\t[%s(...)]' % funcname)
+
+
+if __name__ == '__main__':
+    identify_template('./templates/example.h')
