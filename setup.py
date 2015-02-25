a67a3b9b924f76e906ccc29fe405da99bc5130ee
Luke Olson <luke.olson@gmail.com>
2015-02-24 19:57:25 -0600

    debugging build

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@
 from __future__ import division, print_function, absolute_import
 
 import os
-import sys
-import subprocess
+# import sys
+# import subprocess
+import glob
 
 
 def configuration(parent_package='', top_path=None):
@@ -12,25 +13,31 @@ def configuration(parent_package='', top_path=None):
     config = Configuration()
     config.add_data_dir('tests')
 
-    try:
-        subprocess.check_call([sys.executable, 'instantiate_templates.py',
-                              '--no-force'])
-    except subprocess.CalledProcessError as e:
-        raise Exception('Problem running' + ' '.join(e.cmd))
+    base_headers = [h for h in glob.glob('base/*.h')]
+    template_headers = [h for h in glob.glob('templates/example.h')]
+    depends = base_headers + template_headers
 
-    base_headers = [h for h in dir('./include') if not h.endswith('.h')]
-    template_headers = [h for h in dir('./templates') if not h.endswith('.h')]
-    depends = [os.path.join('include', h) for h in base_headers]
-    depends += [os.path.join('include', h) for h in template_headers]
-    sources = [os.path.join('templates', s.replace('.h', '.cxx'))
-               for s in template_headers]
+    print(base_headers)
+    print(template_headers)
+    print(depends)
+
+    sources = [s.replace('.h', '.cxx') for s in template_headers]
+    sources += [os.path.join('base', 'sparsetools.cxx')]
+    print(sources)
+
+    import generate_functions
+    generate_functions.main(template_headers, '')
+    # try:
+    #     subprocess.check_call([sys.executable, 'generate_functions.py',
+    #                           '--no-force'])
+    # except subprocess.CalledProcessError as e:
+    #     raise Exception('Problem running' + ' '.join(e.cmd))
 
     config.add_extension('crapper',
                          define_macros=[('__STDC_FORMAT_MACROS', 1)],
                          depends=depends,
-                         include_dirs=['include', 'templates'],
+                         include_dirs=['base', 'templates'],
                          sources=sources)
-
     return config
 
 if __name__ == '__main__':
---
+++ templates/example.cxx
@@ -0,0 +1,9 @@
+#define NO_IMPORT_ARRAY
+#define PY_ARRAY_UNIQUE_SYMBOL _scipy_sparse_sparsetools_ARRAY_API
+
+#include "sparsetools.h"
+#include "example.h"
+
+extern "C" {
+#include "example_impl.h"
+}
